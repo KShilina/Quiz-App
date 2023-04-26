d5ae59b82fcad7ce85aea633f07f27f53911b1d9
Ekaterina <yatakshilina@gmail.com>
2023-04-26 10:45:06 -0400

    add queries

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,5 +1,6 @@
 const db = require('../connection');
 
+// get all users
 const getUsers = () => {
   return db.query('SELECT * FROM users;')
     .then(data => {
@@ -7,15 +8,35 @@ const getUsers = () => {
     });
 };
 
-// all quizzes
-const getAllQuizzes = () => {
-  return db.query('SELECT * FROM quizzes;')
-  .then(data => {
-    return data.rows;
-  });
+// get all quizzes
+// const getAllQuizzes = () => {
+//   return db.query('SELECT * FROM quizzes;')
+//   .then(data => {
+//     return data.rows;
+//   });
+// };
+
+/**
+ * Get all quzzes sorted by a single user.
+ * @param {string} owner_id The id of the user.
+ * @return {Promise<[{}]>} A promise to the quizzes.
+ */
+const getAllQuizzes = function (owner_id, limit = 10) {
+  return pool
+    .query(
+      `
+      SELECT owner_id, questions.question
+      FROM quizzes
+      JOIN questions ON quizzes.id = questions.quiz_id
+      LIMIT $2;`,
+      [owner_id, limit]
+    )
+    .then((res) => {
+      return res.rows;
+    });
 };
 
-// all results
+// get all results
 const getAllResults = () => {
   return db.query('SELECT * FROM results;')
   .then(data => {
@@ -23,10 +44,35 @@ const getAllResults = () => {
   });
 };
 
+/**
+ * Add a quiz to the database
+ * @param {{}} quiz An object containing all of the quiz details.
+ * @return {Promise<{}>} A promise to the quiz.
+ */
+
+const addQuiz = function (quiz) {
+  return pool
+    .query(
+      "INSERT INTO quizzes (quiz_url, is_public, owner_id,categories_id, max_questions) VALUES($1, $2, $3, $4, $5 ) RETURNING *",
+      [
+        quiz.quiz_url,
+        quiz.is_public,
+        quiz.owner_id,
+        quiz.categories_id,
+        quiz.max_questions,
+      ]
+    )
+    .then((res) => {
+      console.log(res.rows[0]);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 module.exports = {
   getUsers,
   getAllQuizzes,
   getAllResults,
-
+  addQuiz,
 };
