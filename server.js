dd0bbad82beb2844c27c678849e515ce701abc69
Ekaterina <yatakshilina@gmail.com>
2023-04-27 16:32:50 -0400

    add all end points

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const morgan = require('morgan');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');//use EJS as our template egine
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));//parses body of form request string as object
 app.use(
   '/styles',
   sassMiddleware({
@@ -24,7 +24,7 @@ app.use(
     isSass: false, // false => scss, true => sass
   })
 );
-app.use(express.static('public'));
+app.use(express.static('public'));//serve static files via public directory(CSS, images, etc)
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
@@ -44,40 +44,18 @@ app.use('/users', usersRoutes);
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
+//home page
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/login', (req, res) => {
-  // using encrypted cookies
-  console.log('test');
-  res.render('login.ejs');
-  req.session.user_id = req.params.id;
-});
-
-app.post('/login', (req, res) => {
-  console.log('test');
-  req.session.user_id = req.params.id;
-  res.redirect('login.ejs');//use the post routs only for redirect and put it at the end
-});
-
+//show form page
 app.get('/submit_form', (req, res) => {
-  console.log('submit');
-  res.render('form.ejs');
-});
-
-app.get('/quiz/:id', (req, res) => {
-  console.log('quiz');
-  res.render('quiz.ejs');
-});
-
-app.post('/submit_form', (req, res) => {
-  console.log('submit');
-res.redirect('/');
+  res.render('form');
 });
 
 app.get("/myquizzes", (req, res) => {
-  res.send('This is the existing quizzes page');
+  res.render('quiz');
 });
 
 app.get("/results", (req, res) => {
