50e0255ae7b1984072ea5e018508cdda85ddc5fb
Ekaterina <yatakshilina@gmail.com>
2023-04-29 23:19:44 -0400

    create function takeQuiz

--- db/queries/users.js
+++ db/queries/users.js
@@ -49,8 +49,7 @@ const getAllResults = () => {
  * @return {Promise<{}>} A promise to the quiz.
  */
 
-// add quiz
-
+// add quiz to db
 const addQuiz = function(quiz) {
   //add quiz data to db
   return db
@@ -68,6 +67,21 @@ const addQuiz = function(quiz) {
     .then((res) => {
       return res.rows[0];
     });
+};
+
+//take quiz with an uniq id
+const takeQuiz = function(quiz_id) {
+  return db
+        .query(
+          `SELECT quizzes.title, questions.question, questions.option1,questions.option2, questions.option3, questions.option4
+          FROM quizzes
+          JOIN questions ON quiz_id = quizzes.id
+          WHERE quiz_id = $1 `,
+          [quiz_id]
+        )
+        .then((res) => {
+          return res.rows;
+        });
 };
 
 // adding a question to the quiz
@@ -104,4 +118,5 @@ module.exports = {
   getAllResults,
   addQuiz,
   addQuestion,
+  takeQuiz,
 };
